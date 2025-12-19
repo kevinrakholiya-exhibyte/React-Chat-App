bcfa5146d425b7fe4b3a815df1ed5f21a7b5fc2a
Kevin Rakholiya-Exhibyte <kevin.rakholiya@exhibytesolution.com>
2025-12-19 20:11:54 +0530

    enable edit and delete message

--- ChatApp/src/DB/indexedDB.js
+++ ChatApp/src/DB/indexedDB.js
@@ -58,8 +58,42 @@ export const getUsersFromDB = async () => {
     return new Promise((resolve) => {
         const request = store.getAll();
         request.onsuccess = () => resolve(request.result);
-        request.onerror = () => reject(req.error);
+        request.onerror = () => reject(request.error);
     });
 }
 
+// Update UserProfile
+export const updateUserProfile = async (updatedUser) => {
+    const db = await openDB()
+    const transaction = db.transaction(USER_STORE, "readwrite")
+    const store = transaction.objectStore(USER_STORE);
+    return new Promise((resolve, reject) => {
+        const request = store.put(updatedUser);
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+    })
+}
 
+// Update Message 
+export const updateMessageInDB = async (updatedMessage) => {
+    const db = await openDB()
+    const transaction = db.transaction(MESSAGE_STORE, "readwrite");
+    const store = transaction.objectStore(MESSAGE_STORE);
+    return new Promise((resolve, reject) => {
+        const request = store.put(updatedMessage)
+        request.onsuccess = () => resolve(request.result)
+        request.onerror = () => reject(request.error)
+    })
+}
+
+// Delete Message
+export const deleteMessageFromDB = async (messageId) => {
+    const db = await openDB()
+    const transaction = db.transaction(MESSAGE_STORE, "readwrite")
+    const store = transaction.objectStore(MESSAGE_STORE);
+    return new Promise((resolve, reject) => {
+        const request = store.delete(messageId);
+        request.onsuccess = () => resolve(request.result)
+        request.onerror = () => reject(request.error)
+    })
+}
